95e84ed5e90d34882a6782dab07e5cba10a5ca6a
Adam Zbrowski <adam.zbrowski.programista@gmail.com>
2025-09-18 15:44:31 +0200

    - added product environment; - disable db console info;

---
+++ src/main/java/com/zaxis/ecommerce_app/product/ProductController.java
@@ -0,0 +1,31 @@
+package com.zaxis.ecommerce_app.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService productService;
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return productService.getProductById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return productService.addProduct(product);
+    }
+}
---
+++ src/main/java/com/zaxis/ecommerce_app/product/ProductService.java
@@ -0,0 +1,26 @@
+package com.zaxis.ecommerce_app.product;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ProductService {
+
+    private final ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Optional<Product> getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product addProduct(Product product) {
+        return productRepository.save(product);
+    }
+}
