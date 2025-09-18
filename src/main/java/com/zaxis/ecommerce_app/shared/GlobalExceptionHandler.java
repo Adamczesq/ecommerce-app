3ae1059f764fa26f87a66d626fa12ddd861a9472
Adam Zbrowski <adam.zbrowski.programista@gmail.com>
2025-09-18 20:29:08 +0200

    - added global exception handler;

---
+++ src/main/java/com/zaxis/ecommerce_app/shared/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.zaxis.ecommerce_app.shared;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        return ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(
+                        FieldError::getField,
+                        fieldError -> fieldError.getDefaultMessage() != null ? fieldError.getDefaultMessage() : "No error message"
+                ));
+    }
+}
