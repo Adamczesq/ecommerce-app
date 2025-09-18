90204ae634a45f2471fce6a63ca68a1f43590664
Adam Zbrowski <adam.zbrowski.programista@gmail.com>
2025-09-18 20:38:55 +0200

    - added user details; - updated security config;

--- src/main/java/com/zaxis/ecommerce_app/config/SecurityConfig.java
+++ src/main/java/com/zaxis/ecommerce_app/config/SecurityConfig.java
@@ -2,8 +2,12 @@ package com.zaxis.ecommerce_app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -11,5 +15,20 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/users/register").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/api/products/**").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/api/products").hasRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(basic -> {});
+
+        return http.build();
     }
 }
